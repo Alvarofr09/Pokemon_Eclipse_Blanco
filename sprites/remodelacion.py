9f91671b334b074e53f700364d5ed139836701a7
Alvarofr09 <127234472+Alvarofr09@users.noreply.github.com>
2025-07-20 12:00:55 +0200

    sprites cortados

--- sprites/remodelacion.py
+++ sprites/remodelacion.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
-imagen = Image.open("DS DSi - Pokemon Black White - Pokemon 5th Generation Back.png")
+imagen = Image.open("front.png")
 
-ancho_sprite = 98
-alto_sprite = 104
+ancho_sprite = 96
+alto_sprite = 103
 
-columnas = 7
-filas = 32
+columnas = 8
+filas = 22
 
 contador = 0
 for fila in range(filas):
@@ -16,6 +16,6 @@ for fila in range(filas):
 		x1 = x0 + ancho_sprite
 		y1 = y0 + alto_sprite
 		sprite = imagen.crop((x0, y0, x1, y1))
-		sprite.save(f"back/sprite_{contador + 1}_back.png")
+		sprite.save(f"front/sprite_{contador + 1}_front.png")
 
 		contador += 1
